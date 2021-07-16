85f2cc2c0fd2c42525037024904a123ac96c224f
tuliodev <resendetulio51@gmail.com>
2021-07-16 14:42:10 -0300

    feat: start-project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 
 function App() {
+
+  const [isNewTransactionModalOpen, setisNewTransactionModalOpen ] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setisNewTransactionModalOpen(true);
+  }
+
+
+  function handleCloseNewTransactionModal() {
+      setisNewTransactionModalOpen(false);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <GlobalStyle />
+
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+        
+        <NewTransactionModal 
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+
+        />
+
+        <Dashboard />
+    </>
   );
 }
 
